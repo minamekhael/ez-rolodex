76b12f0107399f434fdcf1b949e742aed0fb8081
Mina Mekhael <mina.mekhael@ymail.com>
2020-01-13 16:14:47 -0800

    adding filed for github pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import {CardList} from './components/card-list/card-list.component'
+import {SearchBox} from './components/search-box/search-box.component'
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      people: [],
+      searchField: ''
+    }
+  }
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({ people: users }))
+  }
+
+  handleChange = e => {
+    this.setState({ searchField: e.target.value })
+  }
+
+render(){
+  const { people, searchField } = this.state;
+  const filteredPeople = people.filter(people=>
+    people.name.toLowerCase().includes(searchField.toLowerCase())
+  )
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>EZ Rolodex</h1>
+      <SearchBox
+        placeholder='search people'
+        handleChange={this.handleChange}
+      />
+      <CardList people={filteredPeople}/>
     </div>
   );
+  }
 }
 
 export default App;
