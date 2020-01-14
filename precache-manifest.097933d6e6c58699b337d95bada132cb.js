588124389e0399b73c90b73b1d3db0708c44f6e3
Mina Mekhael <mina.mekhael@ymail.com>
2020-01-13 16:14:21 -0800

    Updates

---
+++ precache-manifest.097933d6e6c58699b337d95bada132cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ad3085e1b2eb3bcfd5538e7ccd1faf1",
+    "url": "/ez-rolodex/index.html"
+  },
+  {
+    "revision": "28b18391a5f6b6272161",
+    "url": "/ez-rolodex/static/css/main.d25878f5.chunk.css"
+  },
+  {
+    "revision": "c6ae8cabb41c72aeb3d7",
+    "url": "/ez-rolodex/static/js/2.d8785254.chunk.js"
+  },
+  {
+    "revision": "28b18391a5f6b6272161",
+    "url": "/ez-rolodex/static/js/main.fb8abfad.chunk.js"
+  },
+  {
+    "revision": "0b0794937e773b8a82cd",
+    "url": "/ez-rolodex/static/js/runtime-main.85ffc5fe.js"
+  }
+]);
